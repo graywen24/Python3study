2a782e740d9b3af804cec6b7e3e1dc38b5ec21e6
graywen24 <deutschland.gray@gmail.com>
2021-08-27 01:58:53 +0800

    commite update

--- shopee/fio1.py
+++ shopee/fio1.py
@@ -9,7 +9,8 @@ import subprocess
 import json
 from prettytable import PrettyTable
 
-rwResult = {}
+#rwResult = {}
+rwResult2 = {}
 
 
 def bash(cmd):
@@ -39,12 +40,7 @@ def color_print(msg, color='red', exits=False):
     return msg
 
 
-def printResult():
-    table = PrettyTable(["项目", "写IOPS", "写带宽", "读IOPS", "读带宽"])
-    for k, v in rwResult.items():
-        list = [k, v["write_iops"], v["write_bw"], v["read_iops"], v["read_bw"]]
-        table.add_row(list)
-    color_print(table)
+
 
 
 def Usage():
@@ -58,73 +54,9 @@ def Usage():
 
 
 
-class PreSetup(object):
-    def __init__(self):
-        self.dist = platform.linux_distribution()[0].lower()
-    @property
-    def _is_redhat(self):
-        if self.dist.startswith("centos") or self.dist.startswith(
-                "red") or self.dist == "fedora" or self.dist == "amazon linux ami":
-            return True
-    @property
-    def _is_ubuntu(self):
-        if self.dist == "ubuntu" or self.dist == "debian" or self.dist == "deepin":
-            return True
-    def check_platform(self):
-        if not (self._is_redhat or self._is_ubuntu):
-            print(u"支持的平台: CentOS, RedHat, Fedora, Debian, Ubuntu, Amazon Linux, 暂不支持其他平台安装.")
-            exit()
-    @staticmethod
-    def check_bash_return(ret_code, error_msg):
-        if ret_code != 0:
-            color_print(error_msg, 'red')
-            exit()
-    def _set_env(self):
-        #color_print('开始关闭防火墙和selinux', 'green')
-        if self._is_redhat:
-            os.system("export LANG='en_US.UTF-8'")
-            # if self._is_centos7 or self._is_fedora_new:
-            #     cmd1 = "systemctl status firewalld 2> /dev/null 1> /dev/null"
-            #     cmd2 = "systemctl stop firewalld"
-            #     cmd3 = "systemctl disable firewalld"
-            #     bash('%s && %s && %s' % (cmd1, cmd2, cmd3))
-            #     bash('localectl set-locale LANG=en_US.UTF-8')
-            #     bash('which setenforce 2> /dev/null 1> /dev/null && setenforce 0')
-            # else:
-            #     bash("sed -i 's/LANG=.*/LANG=en_US.UTF-8/g' /etc/sysconfig/i18n")
-            #     bash('service iptables stop && chkconfig iptables off && setenforce 0')
-        if self._is_ubuntu:
-            os.system("export LANG='en_US.UTF-8'")
-            # bash("which iptables && iptables -F")
-            # bash('which setenforce && setenforce 0')
-    def _rpm_repo(self):
-        if self._is_redhat:
-            color_print('开始安装epel源', 'green')
-            bash('yum -y fio python-pip')
-    def _depend_rpm(self):
-        color_print('开始安装依赖包', 'green')
-        if self._is_redhat:
-            cmd = 'yum -y install epel-release'
-            ret_code = bash(cmd)
-            self.check_bash_return(ret_code, "安装依赖失败, 请检查安装源是否更新或手动安装！")
-        if self._is_ubuntu:
-            cmd = "apt-get -y --force-yes install fio python-pip"
-            ret_code = bash(cmd)
-            self.check_bash_return(ret_code, "安装依赖失败, 请检查安装源是否更新或手动安装！")
-    def _require_pip(self):
-        color_print('开始安装依赖pip包', 'green')
-        ret_code = bash('pip install prettytable')
-        self.check_bash_return(ret_code, "安装依赖的python库失败！")
-    def start(self):
-        color_print('请务必先查看README.txt')
-        time.sleep(3)
-        self.check_platform()
-        self._rpm_repo()
-        self._depend_rpm()
-        self._require_pip()
-        self._set_env()
 class FioTest(object):
-    def __init__(self,name,filename,rw,bs,size,direct=1,iodepth=1,ioengine="libaio",runtime=60,rwmixwrite=30):
+    def __init__(self,name,filename,rw,bs,size,direct=1,iodepth=1,ioengine="libaio",runtime=60):
+        print("lest start")
         self.name=name
         self.filename=filename
         self.direct=direct
@@ -134,11 +66,13 @@ class FioTest(object):
         self.iodepth=iodepth
         self.ioengine=ioengine
         self.runtime=runtime
-        self.rwmixwrite=rwmixwrite
-    def exprCmd(self):
-       cmd ="fio --name="+self.name+" --rw="+self.rw+" --iodepth="+str(self.iodepth)+" --ioengine="+self.ioengine+" --thread --direct="+str(self.direct)+" --norandommap --bs="+self.bs+" --size="+self.size+" --runtime="+str(self.runtime)+" --filename="+self.filename+ " --output="+self.name+ "-iodepth-" +str(self.iodepth)+ "-" + self.rw + " --minimal --output-format=json"
-       #print(cmd)
+
+    def execCmd(self):
+       cmd ="fio --name="+self.name+" --rw="+self.rw+" --iodepth="+str(self.iodepth)+" --ioengine="+self.ioengine+" --thread --direct="+str(self.direct)+" --norandommap --bs="+self.bs+" --size="+self.size+" --runtime="+str(self.runtime)+" --filename="+self.filename+  " --minimal --output-format=json"
+       #--output="+self.name+ "-iodepth-" +str(self.iodepth)+ "-" + self.rw
+       print(cmd)
        return cmd
+
     def runCmd(self, cmd):
         result_str = ''
         process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -155,6 +89,7 @@ class FioTest(object):
         if errors:
             errors.close()
         return str(result_str)
+
     def explain(self, result):
         if self.rw == "read" or self.rw == "randread":
             r = json.loads(result)
@@ -167,64 +102,122 @@ class FioTest(object):
                 iops = r["jobs"][0]["write"]["iops"]
                 bw = r["jobs"][0]["write"]["bw"]
                 return iops, bw
-    def expResult(self, iops, bw):
+
+    def explain2(self, result):
+            r = json.loads(result)
+            read_iops = r["jobs"][0]["read"]["iops"]
+            print("===read_iops==",self.rw, read_iops)
+            read_bw = r["jobs"][0]["read"]["bw"]
+            print("===read_bw==", self.rw, read_bw)
+            write_iops = r["jobs"][0]["write"]["iops"]
+            print("===write_iops==", self.rw, write_iops)
+            write_bw = r["jobs"][0]["write"]["bw"]
+            print("===write_bw==", self.rw, write_bw)
+            return read_iops,read_bw,write_iops,write_bw
+
+
+
+
+
+    def printResult():
+        table = PrettyTable(["项目", "写IOPS", "写带宽", "读IOPS", "读带宽"])
+        for k, v in rwResult.items():
+            list = [k, v["write_iops"], v["write_bw"], v["read_iops"], v["read_bw"]]
+            table.add_row(list)
+        color_print(table)
+
+
+
+
+    def expResult2(self, read_iops,read_bw,write_iops,write_bw):
         name = ""
-        rw_iops = ""
-        rw_bw = ""
+        read_iops = ""
+        read_bw = ""
+        write_iops = ""
+        write_bw = ""
         rw = self.rw
         if rw == "write" or rw == "read":
-            name = "seq-"+self.bs+"-"+str(self.iodepth)
-            rw_iops = rw+"_iops"
-            rw_bw = rw+"_bw"
+            name = "seq-"+self.rw+"-bs-"+self.bs+"-iodepth-"+str(self.iodepth)
+            read_iops = read_iops
+            read_bw = read_bw
+            write_iops = write_iops
+            write_bw =write_bw
+
+
+
         elif rw == "randwrite" or rw == "randread":
-                name = "rand-"+self.bs+"-"+str(self.iodepth)
-                rw_iops=rw[4:]+"_iops"
-                rw_bw=rw[4:]+"_bw"
+            name = "rand-"+self.rw+"-bs-"+self.bs+"-iodepth-"+str(self.iodepth)
+            read_iops = read_iops
+            read_bw = read_bw
+            write_iops = write_iops
+            write_bw = write_bw
+        elif rw == "randrw":
+            name =  self.rw + "-bs-" + self.bs + "-iodepth-" + str(self.iodepth)
+            read_iops = read_iops
+            read_bw = read_bw
+            write_iops = write_iops
+            write_bw = write_bw
+
+        if name in rwResult2.keys():
+            print("===rwResult2==========",name)
+            rwResult2[name][read_iops] = read_iops
+            rwResult2[name][read_bw] = read_bw
+            rwResult2[name][write_iops] = write_iops
+            rwResult2[name][write_bw] = write_bw
         else:
-            name = "NotDefine"
-            rw_iops=rw+"_iops"
-            rw_bw = rw+"_bw"
-        if name in rwResult.keys():
-            rwResult[name][rw_iops] = iops
-            rwResult[name][rw_bw] = bw
-        else:
-            rwResult[name]={}
-            rwResult[name][rw_iops] = iops
-            rwResult[name][rw_bw] = bw
+            rwResult2[name] = {}
+            rwResult2[name][read_iops] = read_iops
+            rwResult2[name][read_bw] = read_bw
+            rwResult2[name][write_iops] = write_iops
+            rwResult2[name][write_bw] = write_bw
+
+
+
+
     def saveResult(self):
-        cmd = self.exprCmd()
+        cmd = self.execCmd()
         result = self.runCmd(cmd)
-        iops, bw = self.explain(result)
-        self.expResult(iops, bw)
+        read_iops,read_bw,write_iops,write_bw = self.explain2(result)
+        self.expResult2(read_iops,read_bw,write_iops,write_bw)
+
+
+def printResult2():
+    table = PrettyTable(["项目", "写IOPS", "写带宽", "读IOPS", "读带宽"])
+    for k, v in rwResult2.items():
+        print(type(rwResult2))
+        print("========",)
+        list = [k, v["read_iops"], v["read_bw"], v["write_iops"], v["write_bw"]]
+        table.add_row(list)
+    color_print(table)
+
+
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-       Usage()
-       exit(1)
-    size4k = sys.argv[1]
-    size128k = sys.argv[2]
-    filename = sys.argv[3]
-    if not os.path.isfile("/usr/bin/fio") :
-        pre_setup = PreSetup()
-        pre_setup.start()
+    #if len(sys.argv) != 4:
+       #Usage()
+       #exit(1)
+ #   bs = sys.argv[1]
+ #   size = sys.argv[2]
+ #   filename = sys.argv[3]
+
+    bslist = ["4k","8k"]
+    sizelist = ["1m","10m"]
+    filename = "testfile"
+    for bs in bslist:
+        for size in sizelist:
+            cmd1 = FioTest(name="randread-", rw="randread", iodepth=1, ioengine="libaio", direct=1, bs=bs, size=size, runtime=60, filename=filename)
+            cmd1.saveResult()
 
 
-    cmd1 = FioTest(name="randread-4k", rw="randread", iodepth=1, ioengine="libaio", direct=1, bs=sys.argv[1], size= sys.argv[2], runtime=60, filename=filename)
-    #cmd1.saveResult()
-    #print(cmd1.saveResult())
-    cmd2 = FioTest(name="randread-4k", rw="randread", iodepth=4, ioengine="libaio", direct=1, bs=sys.argv[1], size= sys.argv[2], runtime=60, filename=filename)
-    #cmd2.saveResult()
+            cmd1 = FioTest(name="randwrite-"+bs, rw="randwrite", iodepth=1, ioengine="libaio", direct=1, bs=bs, size=size,
+                   runtime=60, filename=filename)
+            cmd1.saveResult()
+            cmd1 = FioTest(name="read-" + bs, rw="read", iodepth=1, ioengine="libaio", direct=1, bs=bs, size=size,
+                   runtime=60, filename=filename)
+            cmd1.saveResult()
+            cmd1 = FioTest(name="write-" + bs, rw="write", iodepth=1, ioengine="libaio", direct=1, bs=bs, size=size,
+                   runtime=60, filename=filename)
+            cmd1.saveResult()
+            printResult2()
 
-    cmd3 = FioTest(name="randread-4k", rw="randread", iodepth=8, ioengine="libaio", direct=1, bs=sys.argv[1], size= sys.argv[2], runtime=60, filename=filename)
-    #cmd3.saveResult()
-    #print(cmd2.saveResult())
 
-    cmd4 = FioTest(name="randwrite-4k", rw="randwrite", iodepth=1, ioengine="libaio", direct=1, bs=sys.argv[1],size=sys.argv[2], runtime=60, filename=filename)
-    #cmd4.saveResult()
-    # print(cmd1.saveResult())
-    cmd5 = FioTest(name="randwrite-4k", rw="randwrite", iodepth=4, ioengine="libaio", direct=1, bs=sys.argv[1],size=sys.argv[2], runtime=60, filename=filename)
-    #cmd5.saveResult()
 
-    cmd6 = FioTest(name="randwrite-4k", rw="randwrite", iodepth=8, ioengine="libaio", direct=1, bs=sys.argv[1],size=sys.argv[2], runtime=60, filename=filename)
-    #cmd6.saveResult()
-
-    printResult()
